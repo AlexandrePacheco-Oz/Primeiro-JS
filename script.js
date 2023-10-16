7380616b91a6d54e5f6209db3fc5027f16b33763
Alexandre Pacheco <ale123pacheco36@gmail.com>
2023-10-16 19:39:35 -0300

    First Comit

---
+++ script.js
@@ -0,0 +1,9 @@
+var nome = prompt("Qual seu nome ?");
+var anoNascimento = prompt("Qual ano você nasceu ?");
+
+var anoAtual = 2023;
+
+var resultado =  anoAtual - anoNascimento ;
+
+alert("Olá, " + nome + ". Sua idade é " + resultado + " Anos");
+
